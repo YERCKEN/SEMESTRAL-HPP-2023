f56346982f5c1546dd1b982ae7086d56eb6b2750
Jeison0770 <jeisonparedes63@gmail.com>
2023-07-21 18:14:52 -0500

    cadena de conexion de jeison

--- SEMESTRAL/VARIABLES_GLOBALES.vb
+++ SEMESTRAL/VARIABLES_GLOBALES.vb
@@ -11,8 +11,8 @@ Module VARIABLES_GLOBALES
     'Public cadenaConexion As String = "Data source =YERCKEN\SQLEXPRESS; Initial Catalog=baseDeDatos2; integrated security = true"
 
     'JEISSON
-    Dim connectionString1 As String = "Data Source=JEISSONPC\SQLEXPRESS;Initial Catalog=baseDeDatos1;Integrated Security=True"
-    Dim connectionString2 As String = "Data Source=JEISSONPC\SQLEXPRESS;Initial Catalog=baseDeDatos2;Integrated Security=True"
+    Public cadenaConexion As String = "Data Source=JEISSONPC\SQLEXPRESS;Initial Catalog=baseDeDatos1;Integrated Security=True"
+    'Public cadenaConexion As String = "Data Source=JEISSONPC\SQLEXPRESS;Initial Catalog=baseDeDatos2;Integrated Security=True"
 
 
 
